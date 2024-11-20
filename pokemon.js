dd0781106ec783dc742e6c61e81cccf16e277aac
Marvintech1 <marcos.s@sou.unifeob.edu.br>
2024-11-20 17:44:42 -0300

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -2,7 +2,6 @@ const pokemonNameElement = document.getElementById('pokemon-name');
 const pokemonImageElement = document.getElementById('pokemon-image');
 const pokemonInfoElement = document.getElementById('pokemon-info');
 
-// Get the Pokémon name from the URL
 const urlParams = new URLSearchParams(window.location.search);
 const pokemonName = urlParams.get('name');
 
@@ -10,11 +9,9 @@ async function fetchPokemonDetails(name) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
     const data = await response.json();
 
-    // Display Pokémon details
     pokemonNameElement.innerText = data.name.charAt(0).toUpperCase() + data.name.slice(1);
     pokemonImageElement.src = data.sprites.front_default;
 
-    // Create a list of details
     const types = data.types.map(typeInfo => typeInfo.type.name).join(', ');
     const height = data.height;
     const weight = data.weight;
@@ -26,10 +23,8 @@ async function fetchPokemonDetails(name) {
     `;
 }
 
-// Function to go back to the previous page
 function goBack() {
     window.history.back();
 }
 
-// Fetch Pokémon details on page load
 fetchPokemonDetails(pokemonName);
